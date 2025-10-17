a1b9d8d4a2537ff2bee3763da8f9166053117029
Soucius <soucius.ggwp@gmail.com>
2025-10-17 17:50:01 +0300

    message part 1

---
+++ backend/src/controllers/message.controller.js
@@ -0,0 +1,19 @@
+import Message from "../models/Message.js";
+import User from "../models/User.js";
+
+export const getAllContacts = async (req, res) => {
+    try {
+        const loggedInUserId = req.user._id;
+        const filteredUsers = await User.find({
+            _id: {
+                $ne: loggedInUserId
+            }
+        }).select("-password");
+
+        res.status(200).json(filteredUsers);
+    } catch (error) {
+        console.error("Error in getAllContacts: ", error);
+        
+        res.status(500).json({ message: "Internal server error" });
+    }
+};
